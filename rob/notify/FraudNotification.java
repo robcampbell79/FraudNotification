dbd2f8614ea548154c1fd6d209d1012102c99297
Higalgo <rhc91310@gmail.com>
2018-10-25 23:37:11 -0500

    Initialize array

--- rob/notify/FraudNotification.java
+++ rob/notify/FraudNotification.java
@@ -1,7 +1,25 @@
 package rob.notify;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FraudNotification {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter number of days and number of trailing days: ");
+		
+		int days = sc.nextInt();
+		int trlDays = sc.nextInt();
+		
+		int arr[] = new int[days];
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print("Enter day " + (i+1) + ": ");
+			int day = sc.nextInt();
+			arr[i] = day;
+		}
+		
+		System.out.println(Arrays.toString(arr));
 	}
 }
